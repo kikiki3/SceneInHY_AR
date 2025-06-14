40c20eb3680b597bd804f88933c85f5ff1d1c828
kikiki3 <cofls2525@gmail.com>
2025-06-15 06:40:40 +0900

    End

---
+++ Assets/Scene3Assets/Scene3Scripts/Scene3TomainScene.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene3TomainScene : MonoBehaviour
+{
+    public void LoadMainScene()
+    {
+        SceneManager.LoadScene("mainScene");
+    }
+}
